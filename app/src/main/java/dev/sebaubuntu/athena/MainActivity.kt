4b8918c7aee772a9a5b886f1c512c68db3ebdd07
IsNevis <94104284+IsNevis@users.noreply.github.com>
2024-10-16 04:40:06 +0530

    Update MainActivity.kt

--- app/src/main/java/dev/sebaubuntu/athena/MainActivity.kt
+++ app/src/main/java/dev/sebaubuntu/athena/MainActivity.kt
@@ -137,8 +137,8 @@ class MainActivity : AppCompatActivity() {
                 .setTitle(R.string.shizukuButton)
                 .setMessage(R.string.shizukuButton_description)
                 .setPositiveButton(R.string.yes) { _, _ ->
-                    // shizukuButton()
-                    startActivity(Intent(this, PermGrantActivity::class.java))
+                    shizukuButton()
+                    // startActivity(Intent(this, PermGrantActivity::class.java))
                     // requestPermissionsContract.launch(
                     //     SectionEnum.values().map {
                     //         it.clazz.requiredPermissions.toList()
@@ -243,7 +243,7 @@ class MainActivity : AppCompatActivity() {
 
         //     withContext(Dispatchers.IO) {
                 if (ContextCompat.checkSelfPermission(this, BATTERY_STATS_PERM) == PackageManager.PERMISSION_GRANTED) {
-                    findViewById<MaterialCardView>(R.id.perm_request).visibility = View.GONE
+                    findViewById<MaterialCardView>(R.id.shizukuButton).visibility = View.GONE
         
                     // Enable extra features
                     // findViewById<LinearLayout>(R.id.extra_health).visibility = View.VISIBLE
@@ -253,7 +253,7 @@ class MainActivity : AppCompatActivity() {
         
                     // updateHiddenData()
                 } else {
-                    findViewById<Button>(R.id.perm_request_button).setOnClickListener {
+                    findViewById<Button>(R.id.shizukuButton).setOnClickListener {
                         startActivity(Intent(this, PermGrantActivity::class.java))
                     }
                 }
